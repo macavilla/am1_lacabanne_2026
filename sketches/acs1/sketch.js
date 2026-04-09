992271937b7fa4cf2aa36a092bda97587bbe1d83
macavilla <maca.villa.a@gmail.com>
2026-04-09 09:54:57 -0300

    ACS1: WIP waldemar cordeiro

--- sketches/acs1/sketch.js
+++ sketches/acs1/sketch.js
@@ -1,21 +1,58 @@
 /**
- * AM1 - TP Nº[X]: [TITULO]
+ * AM1 - ACS1: Waldemar Cordeiro
  * Restricciones: 
  * - Lienzo: 512x512px
- * - Color: Escala de grises (8-bit, 0-255)
  * - Primitivas 2D, Bézier, Vertex solamente.
  * - Sin 3D, WebGL ni imágenes (bitmaps).
  */
 
 // --- CONFIG ---
 const CANVAS_SIZE = 512;
-const MODO_GRISES = 255;
+const COLOR = {
+  rojo: "#aa3625",
+  blanco: "#dfd4c7"
+}
 
+const ARC_GRANDE = {
+  x: CANVAS_SIZE * .5,
+  y: CANVAS_SIZE * .5,
+  w: CANVAS_SIZE * .6,
+  h: CANVAS_SIZE * .6,
+  start: 0,
+  end: 270
+}
+const ARC_IZQ = {
+  x: CANVAS_SIZE * .2,
+  y: CANVAS_SIZE * .5,
+  w: CANVAS_SIZE * .2,
+  h: CANVAS_SIZE * .2,
+  start: 0,
+  end: 180
+}
+const ARC_MASK = {
+  x: CANVAS_SIZE * .82,
+  y: CANVAS_SIZE * .5,
+  w: ARC_GRANDE.w * 1.4,
+  h: ARC_GRANDE.h * 1.4,
+  start: 180,
+  end: 180 + 60
+}
 function setup() {
   createCanvas(CANVAS_SIZE, CANVAS_SIZE);
-  colorMode(RGB, MODO_GRISES); 
-  background(MODO_GRISES);
+  background(COLOR.rojo);
+  angleMode(DEGREES)
   // noLoop(); // para sketches estáticos
+  strokeCap(SQUARE);
+  strokeWeight(7);
+  stroke(COLOR.blanco);
+  fill(COLOR.rojo)
+  // noFill()
+  // arc(x, y, w, h, start, end);
+
+  arc(ARC_GRANDE.x, ARC_GRANDE.y, ARC_GRANDE.w, ARC_GRANDE.h, ARC_GRANDE.start, ARC_GRANDE.end);
+  arc(ARC_IZQ.x, ARC_IZQ.y, ARC_IZQ.w, ARC_IZQ.h, ARC_IZQ.start, ARC_IZQ.end);
+  arc(ARC_MASK.x, ARC_MASK.y, ARC_MASK.w, ARC_MASK.h, ARC_MASK.start, ARC_MASK.end);
+
 }
 
 function draw() {
